a1cf3bfdddf1e5fb5a594f0283611cff8d1bc936
Michael Mulderink <michaelmulderink@Michaels-MacBook-Pro.local>
2019-09-25 15:53:03 -0500

    Moved hook to hook.js

--- home/src/Stories.js
+++ home/src/Stories.js
@@ -1,32 +1,25 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react';
+import { useFetch } from './hooks';
 
 function Stories() {
-  const [stories, setStories] = useState([]);
-
-  useEffect(() => {
-    fetch(`https://news-proxy-server.appspot.com/topstories`)
-      .then(response => response.json())
-      .then(json => {
-        setStories(json)
-      })
-  }, [])
+  const stories = useFetch(`https://news-proxy-server.appspot.com/topstories`, []);
 
   return (
     <div className='Stories'>
       <h3>Stories</h3>
       {
         stories.map(story => {
-          const {id, by, time, title, url } = story;
+          const { id, by, time, title, url } = story;
           return (
             <div key={id}>
               <a target="_blank" rel="noopener noreferrer" href={url} >{title}</a>
               <div>{by} - {new Date(time * 1000).toLocaleDateString()}</div>
             </div>
-          )
+          );
         })
-      }
+      };
     </div>
-  )
+  );
 }
 
 export default Stories;
