836445b2b1bfefc56f987dbf9c96dc76df564d92
Michael Mulderink <michaelmulderink@Michaels-MacBook-Pro.local>
2019-09-27 16:43:02 -0500

    Added custom hook

--- home/src/hooks.js
+++ home/src/hooks.js
@@ -7,8 +7,24 @@ export const useFetch = (url, initialValue) => {
     fetch(url)
       .then(response => response.json())
       .then(json => setResult(json))
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
   return result;
-}
+};
+
+export const useDynamicTransition = ({ increment, delay, length }) => {
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex(storedIndex => {
+        return (storedIndex + increment) % length;
+      });
+    }, delay);
+    return () => {
+      clearInterval(interval);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [delay, increment]);
+  return index;
+};
