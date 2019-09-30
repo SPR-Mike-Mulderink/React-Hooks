a026ea55b9ea374963fa9b4aa3ad5fd5766951ba
Michael Mulderink <michaelmulderink@Michaels-MacBook-Pro.local>
2019-09-30 13:30:13 -0500

    Brought in store

--- reaction/src/App.js
+++ reaction/src/App.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+import reducer, { initialState } from './state/reducer';
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
     <div className="App">
       <h2>Reaction</h2>
     </div>
   );
-}
+};
 
 export default App;
