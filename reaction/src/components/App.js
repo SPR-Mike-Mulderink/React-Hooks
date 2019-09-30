14c573e100b57866aee8dad2840f3dd565bfef43
Michael Mulderink <michaelmulderink@Michaels-MacBook-Pro.local>
2019-09-30 13:37:33 -0500

    Added components folder and moved App.js into it

---
+++ reaction/src/components/App.js
@@ -0,0 +1,14 @@
+import React, { useReducer } from 'react';
+import reducer, { initialState } from '../state/reducer';
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <div className="App">
+      <h2>Reaction</h2>
+    </div>
+  );
+};
+
+export default App;
